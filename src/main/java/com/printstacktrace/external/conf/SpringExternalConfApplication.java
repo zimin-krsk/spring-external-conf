9e07d9a16ba97ea1c5b4be99b5f6f019ad95e5de
Aleksandr Zimin <zimin.krsk@gmail.com>
2020-08-19 00:17:39 +0300

    Initial message from start.spring.io

---
+++ src/main/java/com/printstacktrace/external/conf/SpringExternalConfApplication.java
@@ -0,0 +1,13 @@
+package com.printstacktrace.external.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExternalConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExternalConfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printstacktrace/external/conf/SpringExternalConfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printstacktrace.external.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExternalConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
